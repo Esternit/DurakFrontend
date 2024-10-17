20d5943ca4a5bc3c10f3a41b71f1168ee34e6831
karpov.dmitrii <dev@karpov.d.a3>
2024-10-17 10:17:04 +0300

    changed pusher

--- pusher.ps1
+++ pusher.ps1
@@ -1,12 +1,12 @@
 param (
-    [string]$commitMessage = "Updated files"
+    [string]$cM = "Updated files"
 )
 
 Write-Host "Adding changes to staging area..."
 git add .
 
-Write-Host "Committing changes with message: '$commitMessage'..."
-git commit -m $commitMessage
+Write-Host "Committing changes with message: '$cM'..."
+git commit -m $cM
 
 Write-Host "Pushing changes to branch"
 git push
