bef52412ce6fe476771100f922715c8e05186ba5
SemenOtvesov <smen.otvrsov@mail.ru>
2024-11-07 15:45:02 +0300

    addWs

--- src/connectToSocket.js
+++ src/connectToSocket.js
@@ -1,44 +1,22 @@
 import config from "./config";
-import { Client } from "@stomp/stompjs";
+import { Stomp } from "@stomp/stompjs";
 import SockJS from "sockjs-client";
 
 export const connectToSocket = (gameId, onChangeFunction) => {
-	// let stompClient = null;
-	let socket = new WebSocket(
-		config.url + "/gameplay?X-Session=" +JSON.parse(localStorage.getItem("user")).id
+	let stompClient = null;
+	let socket = new SockJS(
+	config.url +
+		"/gameplay?X-Session=" +
+		JSON.parse(localStorage.getItem("user")).id
 	);
-	socket.onopen = function (event) {
-		console.log(event)
-        return true;
-    };
-	socket.onmessage = e =>{
-		console.log(e)
-		onChangeFunction(e)
-	};
-	socket.onclose = function (event) {
-		console.log(event)
-        connectionState = false;
-    };
-	// stompClient = Stomp.over(socket);
-	// stompClient.connect({}, function (frame) {
-	// 	stompClient.subscribe(`/topic/game-progress/${gameId}`, response=> {
-	// 			let data = JSON.parse(response.body);
-	// 			onChangeFunction(data);
-	// 		}
-	// 	);
-	// });
-
-	// const client = new Client({
-	// 	brokerURL: config.url + "/gameplay?X-Session=" +JSON.parse(localStorage.getItem("user")).id,
-	// 	onConnect: () => {
-	// 	  client.subscribe(`/topic/game-progress/${gameId}`, message =>
-	// 		console.log(`Received: ${message.body}`)
-	// 	  );
-	// 	  client.publish({ destination: `/topic/game-progress/${gameId}`, body: 'First Message' });
-	// 	},
-	//   });
-	  
-	//   client.activate();
+	stompClient = Stomp.over(socket);
+	stompClient.connect({}, function (frame) {
+		stompClient.subscribe(`/topic/game-progress/${gameId}`, response=> {
+				let data = JSON.parse(response.body);
+				onChangeFunction(data);
+			}
+		);
+	});
 };
 
 
